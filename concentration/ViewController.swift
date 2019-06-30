67a7f0a91873cadf8d10fd66c20e9095e451f7d4
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-06-30 20:37:54 +0200

    Adding some labels

---
+++ concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  concentration
+//
+//  Created by Winston on 6/30/19.
+//  Copyright © 2019 Winston. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
