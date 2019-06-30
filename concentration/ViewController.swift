51e004380e486c74e0b262ab1479d65bb5b2ad2d
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-06-30 22:02:03 +0200

    Setup the flip cards

--- concentration/ViewController.swift
+++ concentration/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Setup FlipCount
+    var flipCount = 0 { didSet{ flipCountlabel.text = "Flips: \(flipCount)" } }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var cardButtons: [UIButton]!
+
+    @IBOutlet weak var flipCountlabel: UILabel!
+    
+    //Emoji array.
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton){
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of:sender){
+            flipCard()
+        } else {
+            print("Chosen card was not in cardButton")
+        }
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = .orange
+        }
+    }
+    
 }
 
