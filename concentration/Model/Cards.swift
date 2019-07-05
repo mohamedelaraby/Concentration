513254a781756af8df115dd32c9dbfc75a29d68c
Muhammad Alaraby <mohamedelaraby1995@gmail.com>
2019-07-05 11:09:26 +0200

    Using MVC design pattern

---
+++ concentration/Model/Cards.swift
@@ -0,0 +1,15 @@
+//
+//  Cards.swift
+//  concentration
+//
+//  Created by Winston on 7/5/19.
+//  Copyright © 2019 Winston. All rights reserved.
+//
+
+import Foundation
+
+struct Cards {
+     var isFacedUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ concentration/Model/Concentration.swift
@@ -0,0 +1,19 @@
+//
+//  Concentration.swift
+//  concentration
+//
+//  Created by Winston on 7/5/19.
+//  Copyright © 2019 Winston. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+    //Inilaize an empty array of cards.
+    var cards = [Cards] ()
+    
+    func chooseCards(at index: Int){
+        
+    }
+}
